d2da7e5fd4c20c315237d9f6173d67bd85e50c47
Bobojon <bqaroev@velox.biz>
2021-02-19 18:17:52 +0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "category",
+    pathMatch: "full",
+  },
+  {
+    path: "folder/:id",
+    loadChildren: () =>
+      import("./folder/folder.module").then((m) => m.FolderPageModule),
+  },
+  {
+    path: "category",
+    loadChildren: () =>
+      import("./category/category.module").then((m) => m.CategoryPageModule),
+  },
+  {
+    path: "person",
+    loadChildren: () =>
+      import("./person/person.module").then((m) => m.PersonPageModule),
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
